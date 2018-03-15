aedf6fe59370ab044c8394101c11f07400d7b08b
bathulah <bathulahmahir@gmail.com>
2018-03-15 17:48:20 +0800

    add service provider

---
+++ src/AuthenticationOfAPIServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Batmahir\OAuth2;
+
+use Illuminate\Support\ServiceProvider;
+
+class AuthenticationOfAPIServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+
+}
